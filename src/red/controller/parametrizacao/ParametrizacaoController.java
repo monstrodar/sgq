755511e18169fe5bece9e67c59c6f42b6c573c7f
monstrodar <39282507+monstrodar@users.noreply.github.com>
2018-10-25 14:28:09 -0300

    Alteração na parametrização

--- src/red/controller/parametrizacao/ParametrizacaoController.java
+++ src/red/controller/parametrizacao/ParametrizacaoController.java
@@ -24,21 +24,7 @@ public class ParametrizacaoController {
     @FXML
     private AnchorPane painelTotal;
     @FXML
-    private VBox painelEsquerda;
-    @FXML
-    private TextField txpesquisa;
-    @FXML
-    private ListView<?> lvcolaboradores;
-    @FXML
     private VBox pnDireita;
-    @FXML
-    private RadioButton rbbasico;
-    @FXML
-    private ToggleGroup gruporadio;
-    @FXML
-    private RadioButton rbauditor;
-    @FXML
-    private RadioButton rbadministrador;
     @FXML
     private TextField txnome;
     @FXML
@@ -46,17 +32,22 @@ public class ParametrizacaoController {
     @FXML
     private TextField txcpf;
     @FXML
-    private TextField txobs;
+    private TextField txnome1;
     @FXML
-    private DatePicker dpdata;
+    private TextField txcpf1;
+    @FXML
+    private TextField txnome11;
+    @FXML
+    private TextField txcargo1;
+    @FXML
+    private TextField txcpf11;
+    @FXML
+    private TextField txcpf111;
 
     @FXML
     private void evtCadColaborador(ActionEvent event) {
     }
 
-    @FXML
-    private void clkBuscar(ActionEvent event) {
-    }
 
     @FXML
     private void clkAlterar(ActionEvent event) {
